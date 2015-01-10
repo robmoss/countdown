cc50a1b24262d4cd4fc59d896d75a51d805c24e2
Rob Moss <robm.dev@gmail.com>
2015-01-10 17:40:56 +1100

    Fix the indentation of an ocamldoc comment

--- src/countdown.ml
+++ src/countdown.ml
@@ -35,7 +35,7 @@ module Dictionary = struct
       if i < 0 then l else exp (i - 1) (s.[i] :: l) in
     exp (String.length s - 1) []
 
-(** Convert a list of characters to a string. *)
+  (** Convert a list of characters to a string. *)
   let implode l =
     let res = String.create (List.length l) in
     let rec imp i = function
